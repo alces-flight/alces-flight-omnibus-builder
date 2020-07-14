597a54f83e4a1647582d830c3bc6d357732e5905
Ben Armston <ben.armston@alces-software.com>
2020-07-14 15:27:42 +0100

    Bump flight-account to 1.3.2

--- builders/flight-account/config/projects/flight-account.rb
+++ builders/flight-account/config/projects/flight-account.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Account'
 
 install_dir '/opt/flight/opt/account'
 
-VERSION = '1.3.1'
+VERSION = '1.3.2'
 override 'flight-account', version: VERSION
 
 build_version VERSION
