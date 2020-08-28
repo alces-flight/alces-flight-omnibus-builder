882d83575ec529fa5e21fa7c2af4a30d1176003e
William McCumsite <william.mccumstie@gmail.com>
2020-08-28 16:58:26 +0100

    Bump flight-asset to version 2.0.0-rc2

--- builders/flight-asset/config/projects/flight-asset.rb
+++ builders/flight-asset/config/projects/flight-asset.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Asset'
 
 install_dir '/opt/flight/opt/asset'
 
-VERSION = '2.0.0-rc1'
+VERSION = '2.0.0-rc2'
 override 'flight-asset-cli', version: VERSION
 
 build_version VERSION
