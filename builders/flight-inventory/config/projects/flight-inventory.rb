98b376032169574cb654caee113fef630c5bffa2
Ben Armston <ben.armston@alces-software.com>
2020-09-10 11:46:02 +0100

    Bump flight-inventory version: 2.1.0

--- builders/flight-inventory/config/projects/flight-inventory.rb
+++ builders/flight-inventory/config/projects/flight-inventory.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Inventory'
 
 install_dir '/opt/flight/opt/inventory'
 
-VERSION = '2.0.0'
+VERSION = '2.1.0'
 override 'flight-inventory', version: VERSION
 
 build_version VERSION
