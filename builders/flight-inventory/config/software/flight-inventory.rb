d7f8e63047aa6b1323001c74718aa50af57deefc
William McCumsite <william.mccumstie@gmail.com>
2020-10-27 13:37:00 +0000

    Upgrade flight-inventory-2.2.0~rc2 to be configurable

--- builders/flight-inventory/config/software/flight-inventory.rb
+++ builders/flight-inventory/config/software/flight-inventory.rb
@@ -1,5 +1,7 @@
+# NOTE: The following copyright header applies to this software config
+COPYRIGHT_HEADER = <<~HEADER.chomp
 #==============================================================================
-# Copyright (C) 2019-present Alces Flight Ltd.
+# Copyright (C) 2020-present Alces Flight Ltd.
 #
 # This file is part of OpenFlight Omnibus Builder.
 #
@@ -24,6 +26,8 @@
 # For more information on OpenFlight Omnibus Builder, please visit:
 # https://github.com/openflighthpc/openflight-omnibus-builder
 #===============================================================================
+HEADER
+
 name 'flight-inventory'
 default_version '0.0.0'
 
@@ -53,4 +57,22 @@ build do
     '--path vendor'
   ].join(' ')
   command "cd #{install_dir} && /opt/flight/bin/bundle install #{flags}", env: env
+
+  # Write the flight config
+  block do
+    File.write File.join(install_dir, 'etc/10-flight.conf'), <<~CONF
+#{COPYRIGHT_HEADER}
+
+#===============================================================================
+# Define the application as part of the flight ecosystem
+#===============================================================================
+@program_name     = ENV.fetch('FLIGHT_PROGRAM_NAME', 'flight inventory')
+@program_version  = '#{version}'
+
+#===============================================================================
+# Store the data with the user's XDG cache home
+#===============================================================================
+@yaml_dir = XDG::Environment.new.cache_home.join("flight/inventory")
+CONF
+  end
 end
