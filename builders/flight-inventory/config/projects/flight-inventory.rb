612f1016da560660b05f3aa107d1ce017d39d430
William McCumsite <william.mccumstie@gmail.com>
2020-11-03 17:21:17 +0000

    Bump flight-inventory to version 2.2.0~rc5

--- builders/flight-inventory/config/projects/flight-inventory.rb
+++ builders/flight-inventory/config/projects/flight-inventory.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Inventory'
 
 install_dir '/opt/flight/opt/inventory'
 
-VERSION = '2.2.0-rc4'
+VERSION = '2.2.0-rc5'
 override 'flight-inventory', version: VERSION
 
 build_version VERSION
