36e5acb43a79a1304963769a8f8c162df4283f33
Ben Armston <ben.armston@alces-software.com>
2020-11-05 15:28:34 +0000

    Bump flight-asset to 2.1.0

--- builders/flight-asset/config/projects/flight-asset.rb
+++ builders/flight-asset/config/projects/flight-asset.rb
@@ -31,7 +31,7 @@ friendly_name 'Flight Asset'
 
 install_dir '/opt/flight/opt/asset'
 
-VERSION = '2.1.0-rc1'
+VERSION = '2.1.0'
 override 'flight-asset-cli', version: VERSION
 
 build_version VERSION
