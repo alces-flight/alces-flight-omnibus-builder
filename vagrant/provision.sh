b64452e5b408831e2aae190cc9c6ad1640c0003a
William McCumsite <william.mccumstie@gmail.com>
2021-07-14 17:20:20 +0100

    Use 'rvm.io' to download the gpg2 keys

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -26,7 +26,8 @@
 # https://github.com/alces-flight/alces-flight-omnibus-builder
 #===============================================================================
 yum install -y -e0 git rpm-build cmake
-gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install 2.6.5
